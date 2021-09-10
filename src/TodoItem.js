641e7a7927de8f85f3847c69db204c2c78838f4c
EC2 Default User <ec2-user@ip-172-31-23-4.eu-central-1.compute.internal>
2021-09-10 17:46:01 +0000

    Added GET and POST api requests

---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+const TodoItem = ({name, completed }) => (
+    <li style={{
+        textDecoration: completed ? 'line-through': 'none'
+    }}>
+        {name}
+    </li>
+);
+
+export default TodoItem;
